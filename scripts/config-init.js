5dd0a0ffa756779de4b819bd7578a08db28fb305
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-05-21 10:45:17 +0200

    initial

---
+++ scripts/config-init.js
@@ -0,0 +1,27 @@
+function displayErrors(fb) {
+    if(fb.errors.length > 0) {
+        alert(fb.errors[0]);
+    }
+} 
+
+function init(initial){
+    var parson = new ParsonsWidget({
+        'sortableId': 'sortable',
+        'trashId': 'sortableTrash',
+        'max_wrong_lines': 1,
+        'feedback_cb' : displayErrors
+    });
+    parson.init(initial);
+    parson.shuffleLines();
+    $("#newInstanceLink").click(function(event){
+        event.preventDefault();
+        parson.shuffleLines();
+    });
+    $("#feedbackLink").click(function(event){
+        event.preventDefault();
+        parson.getFeedback();
+    });
+};
+
+
+init(challenge);
